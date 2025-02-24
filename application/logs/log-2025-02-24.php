9a90037e47a2ffd2de31674bcd800c94fca8e45b
Nandeeshjk01 <nandeesh@medhatech.in>
2025-02-24 17:51:40 +0530

    .

---
+++ application/logs/log-2025-02-24.php
@@ -0,0 +1,58 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2025-02-24 13:17:31 --> Config Class Initialized
+INFO - 2025-02-24 13:17:31 --> Hooks Class Initialized
+DEBUG - 2025-02-24 13:17:31 --> UTF-8 Support Enabled
+INFO - 2025-02-24 13:17:31 --> Utf8 Class Initialized
+INFO - 2025-02-24 13:17:31 --> URI Class Initialized
+INFO - 2025-02-24 13:17:31 --> Router Class Initialized
+INFO - 2025-02-24 13:17:31 --> Output Class Initialized
+INFO - 2025-02-24 13:17:31 --> Security Class Initialized
+DEBUG - 2025-02-24 13:17:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-24 13:17:31 --> Input Class Initialized
+INFO - 2025-02-24 13:17:31 --> Language Class Initialized
+INFO - 2025-02-24 13:17:31 --> Loader Class Initialized
+INFO - 2025-02-24 13:17:31 --> Helper loaded: url_helper
+INFO - 2025-02-24 13:17:31 --> Helper loaded: json_output_helper
+INFO - 2025-02-24 13:17:31 --> Helper loaded: file_helper
+INFO - 2025-02-24 13:17:31 --> Helper loaded: custom_helper
+INFO - 2025-02-24 13:17:31 --> Database Driver Class Initialized
+DEBUG - 2025-02-24 13:17:31 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-24 13:17:31 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-24 13:17:31 --> Table Class Initialized
+INFO - 2025-02-24 13:17:31 --> Helper loaded: form_helper
+INFO - 2025-02-24 13:17:31 --> Form Validation Class Initialized
+INFO - 2025-02-24 13:17:31 --> Controller Class Initialized
+INFO - 2025-02-24 13:17:31 --> Model "Admin_model" initialized
+DEBUG - 2025-02-24 13:17:31 --> Table class already loaded. Second attempt ignored.
+DEBUG - 2025-02-24 13:17:31 --> Form_validation class already loaded. Second attempt ignored.
+ERROR - 2025-02-24 17:47:31 --> Severity: Warning --> Undefined variable $usn D:\Xampp\htdocs\Non_NEP_Certificates\application\views\admin\template\admin_header.php 63
+INFO - 2025-02-24 17:47:31 --> File loaded: D:\Xampp\htdocs\Non_NEP_Certificates\application\views\admin/template/admin_header.php
+INFO - 2025-02-24 17:47:31 --> File loaded: D:\Xampp\htdocs\Non_NEP_Certificates\application\views\admin/dashboard.php
+INFO - 2025-02-24 17:47:31 --> File loaded: D:\Xampp\htdocs\Non_NEP_Certificates\application\views\admin/template/admin_footer.php
+INFO - 2025-02-24 17:47:31 --> Final output sent to browser
+DEBUG - 2025-02-24 17:47:31 --> Total execution time: 0.0304
+INFO - 2025-02-24 13:17:35 --> Config Class Initialized
+INFO - 2025-02-24 13:17:35 --> Hooks Class Initialized
+INFO - 2025-02-24 13:17:35 --> Config Class Initialized
+INFO - 2025-02-24 13:17:35 --> Hooks Class Initialized
+DEBUG - 2025-02-24 13:17:35 --> UTF-8 Support Enabled
+INFO - 2025-02-24 13:17:35 --> Utf8 Class Initialized
+DEBUG - 2025-02-24 13:17:35 --> UTF-8 Support Enabled
+INFO - 2025-02-24 13:17:35 --> Utf8 Class Initialized
+INFO - 2025-02-24 13:17:35 --> URI Class Initialized
+INFO - 2025-02-24 13:17:35 --> URI Class Initialized
+INFO - 2025-02-24 13:17:35 --> Router Class Initialized
+INFO - 2025-02-24 13:17:35 --> Router Class Initialized
+INFO - 2025-02-24 13:17:35 --> Output Class Initialized
+INFO - 2025-02-24 13:17:35 --> Output Class Initialized
+INFO - 2025-02-24 13:17:35 --> Security Class Initialized
+DEBUG - 2025-02-24 13:17:35 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-24 13:17:35 --> Input Class Initialized
+INFO - 2025-02-24 13:17:35 --> Security Class Initialized
+INFO - 2025-02-24 13:17:35 --> Language Class Initialized
+ERROR - 2025-02-24 13:17:35 --> 404 Page Not Found: Assets/js
+DEBUG - 2025-02-24 13:17:35 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-24 13:17:35 --> Input Class Initialized
+INFO - 2025-02-24 13:17:35 --> Language Class Initialized
+ERROR - 2025-02-24 13:17:35 --> 404 Page Not Found: Assets/plugins
