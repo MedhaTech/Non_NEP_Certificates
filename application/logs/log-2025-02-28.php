ee7855c3f2eba04407cc6adf0fd4ef5013b4a261
Nandeeshjk01 <nandeesh@medhatech.in>
2025-02-28 10:56:04 +0530

    change

--- application/logs/log-2025-02-28.php
+++ application/logs/log-2025-02-28.php
@@ -1616,3 +1616,60 @@ INFO - 2025-02-28 10:30:52 --> File loaded: D:\Xampp\htdocs\Non_NEP_Certificates
 INFO - 2025-02-28 10:30:52 --> File loaded: D:\Xampp\htdocs\Non_NEP_Certificates\application\views\template/footer.php
 INFO - 2025-02-28 10:30:52 --> Final output sent to browser
 DEBUG - 2025-02-28 10:30:52 --> Total execution time: 0.0491
+INFO - 2025-02-28 06:09:21 --> Config Class Initialized
+INFO - 2025-02-28 06:09:21 --> Hooks Class Initialized
+DEBUG - 2025-02-28 06:09:21 --> UTF-8 Support Enabled
+INFO - 2025-02-28 06:09:21 --> Utf8 Class Initialized
+INFO - 2025-02-28 06:09:21 --> URI Class Initialized
+INFO - 2025-02-28 06:09:21 --> Router Class Initialized
+INFO - 2025-02-28 06:09:21 --> Output Class Initialized
+INFO - 2025-02-28 06:09:21 --> Security Class Initialized
+DEBUG - 2025-02-28 06:09:21 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-28 06:09:21 --> Input Class Initialized
+INFO - 2025-02-28 06:09:21 --> Language Class Initialized
+INFO - 2025-02-28 06:09:21 --> Loader Class Initialized
+INFO - 2025-02-28 06:09:21 --> Helper loaded: url_helper
+INFO - 2025-02-28 06:09:21 --> Helper loaded: json_output_helper
+INFO - 2025-02-28 06:09:21 --> Helper loaded: file_helper
+INFO - 2025-02-28 06:09:21 --> Helper loaded: custom_helper
+INFO - 2025-02-28 06:09:21 --> Database Driver Class Initialized
+DEBUG - 2025-02-28 06:09:21 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-28 06:09:21 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-28 06:09:21 --> Table Class Initialized
+INFO - 2025-02-28 06:09:21 --> Helper loaded: form_helper
+INFO - 2025-02-28 06:09:21 --> Form Validation Class Initialized
+INFO - 2025-02-28 06:09:21 --> Controller Class Initialized
+INFO - 2025-02-28 06:09:21 --> Model "Admin_model" initialized
+DEBUG - 2025-02-28 06:09:21 --> Table class already loaded. Second attempt ignored.
+DEBUG - 2025-02-28 06:09:21 --> Form_validation class already loaded. Second attempt ignored.
+INFO - 2025-02-28 06:09:21 --> Config Class Initialized
+INFO - 2025-02-28 06:09:21 --> Hooks Class Initialized
+DEBUG - 2025-02-28 06:09:21 --> UTF-8 Support Enabled
+INFO - 2025-02-28 06:09:21 --> Utf8 Class Initialized
+INFO - 2025-02-28 06:09:21 --> URI Class Initialized
+INFO - 2025-02-28 06:09:21 --> Router Class Initialized
+INFO - 2025-02-28 06:09:21 --> Output Class Initialized
+INFO - 2025-02-28 06:09:21 --> Security Class Initialized
+DEBUG - 2025-02-28 06:09:21 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-28 06:09:21 --> Input Class Initialized
+INFO - 2025-02-28 06:09:21 --> Language Class Initialized
+INFO - 2025-02-28 06:09:21 --> Loader Class Initialized
+INFO - 2025-02-28 06:09:21 --> Helper loaded: url_helper
+INFO - 2025-02-28 06:09:21 --> Helper loaded: json_output_helper
+INFO - 2025-02-28 06:09:21 --> Helper loaded: file_helper
+INFO - 2025-02-28 06:09:21 --> Helper loaded: custom_helper
+INFO - 2025-02-28 06:09:21 --> Database Driver Class Initialized
+DEBUG - 2025-02-28 06:09:21 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-28 06:09:21 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-28 06:09:21 --> Table Class Initialized
+INFO - 2025-02-28 06:09:21 --> Helper loaded: form_helper
+INFO - 2025-02-28 06:09:21 --> Form Validation Class Initialized
+INFO - 2025-02-28 06:09:21 --> Controller Class Initialized
+INFO - 2025-02-28 06:09:21 --> Model "Admin_model" initialized
+DEBUG - 2025-02-28 06:09:21 --> Table class already loaded. Second attempt ignored.
+DEBUG - 2025-02-28 06:09:21 --> Form_validation class already loaded. Second attempt ignored.
+INFO - 2025-02-28 10:39:21 --> File loaded: D:\Xampp\htdocs\Non_NEP_Certificates\application\views\template/header.php
+INFO - 2025-02-28 10:39:21 --> File loaded: D:\Xampp\htdocs\Non_NEP_Certificates\application\views\admin/login.php
+INFO - 2025-02-28 10:39:21 --> File loaded: D:\Xampp\htdocs\Non_NEP_Certificates\application\views\template/footer.php
+INFO - 2025-02-28 10:39:21 --> Final output sent to browser
+DEBUG - 2025-02-28 10:39:21 --> Total execution time: 0.0514
