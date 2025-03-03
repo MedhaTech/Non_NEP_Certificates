31c8c45716ed3f131ca23173a21d01eff1441c03
Nandeesh J K <nandeesh@medhatech.in>
2025-03-03 12:26:26 +0530

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -6,8 +6,8 @@ $config['smtp_host'] = 'smtp.gmail.com'; // Replace with your SMTP server sandbo
 $config['smtp_user'] = 'roreplayreplay@gmail.com'; // Replace with your SMTP email d4d05e583650c9
 $config['smtp_pass'] = 'ygfljkibrbnpoaqj'; 
 // Replace with your SMTP password 2203dbcc9f95c3 ygfljkibrbnpoaqj
-$config['smtp_port'] = 587; // Use 465 for SSL or 587 for TLS
-$config['smtp_crypto'] = 'tls'; // Use 'ssl' if needed
+$config['smtp_port'] = 465; // Use 465 for SSL or 587 for TLS
+$config['smtp_crypto'] = 'ssl'; // Use 'ssl' if needed
 $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
 $config['wordwrap'] = TRUE;
